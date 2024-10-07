ca1ca15507436b0aa6c7320bbb613805f85d595f
student-d-k <daumantas.kudzma@codeacademylt.onmicrosoft.com>
2024-10-07 14:26:52 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+
+class Calculator:
+    def __init__(self, symbol: str, number: float) -> None:
+        self.symbol = symbol
+        self.number = number
+
+    def add(self) -> float:
+        ...
+
+    def sub(self) -> float:
+        ...
+
+    def div(self) -> float:
+        ...
+
+    def mul(self) -> float:
+        ...
+
+    def calculate(self) -> float:
+        ...
+ 
